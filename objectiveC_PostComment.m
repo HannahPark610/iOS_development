ca7d3a446f66d6d8f29d8d64a1cd3c96a4d2069c
Hannah Park <teyang610@gmail.com>
2017-02-21 21:15:21 -0800

    Create objectiveC_PostComment.m

---
+++ objectiveC_PostComment.m
@@ -0,0 +1,28 @@
+//
+//  PostComment.m
+//  
+//
+//  Created by HannahPark on 2017-02-21.
+//
+//
+
+#import "PostComment.h"
+
+@implementation PostComment
+
+-(id) initWithCommentID:(int)commentID commentAuthor:(User*)commentAuthor dte:(NSDate*)date attachments:(NSMutableArray<Attachment*>*)attachments
+{
+    self = [super init];
+    
+    if(self)
+    {
+        self.commentID = commentID;
+        self.commentAuthor = commentAuthor;
+        self.date = date;
+        self.attachments = attachments;
+    }
+    
+    return self;
+}
+
+@end
