ccb3f5e7cf15620de6a1f4f67ea1b6a123186e37
Hannah Park <teyang610@gmail.com>
2017-02-21 21:14:35 -0800

    Create objectiveC_Like.m

---
+++ objectiveC_Like.m
@@ -0,0 +1,13 @@
+//
+//  Like.m
+//  Linkedin
+//
+//  Created by HannahPark on 2017-02-21.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import "Like.h"
+
+@implementation Like
+
+@end
