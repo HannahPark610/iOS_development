34008ba2999307de35d607144ed4e7d5617a7a7c
Hannah Park <teyang610@gmail.com>
2017-02-21 21:12:39 -0800

    Create objectiveC_User.h

---
+++ objectiveC_User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  Linkedin
+//
+//  Created by HannahPark on 2017-02-21.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
