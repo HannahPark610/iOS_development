d033b3d90754e291f673cd7e0717b454cfcfaad3
Hannah Park <teyang610@gmail.com>
2017-02-22 21:33:18 -0800

    Update objectiveC_profile.m

--- objectiveC_profile.m
+++ objectiveC_profile.m
@@ -6,25 +6,62 @@
 //  Copyright © 2017 HannahPark. All rights reserved.
 //
 
-#import "Account.h"
+#import "Profile.h"
 
-@implementation Account
+@implementation Profile
 
--(id) initWithUsername:(NSString*)myUsername password:(NSString*)myPassword
+-(id) initWithFirstname:(NSString*)firstname lastname:(NSString*)lastname address:(NSString*)address age:(int)age phoneNumber:(NSString*)phoneNumber profilePicURL:(NSString*)profilePicURL
 {
-    self = [super init]; //allocated some memory
+    //step1: memory allocation
+    self = [super init];
+    
+    //step2: properties initialization
     if(self)
     {
-        self.username = myUsername;
-        self.password = myPassword;
+        self.firstname = firstname;
+        self.lastname = lastname;
+        self.address = address;
+        self.age = age;
+        self.phoneNumber = phoneNumber;
+        
+        self.profilePicURL = profilePicURL;
     }
+    
     return self;
 }
 
 
--(BOOL) isAccountValid
+-(void) editProfileWithNewfirstname:(NSString*)newFirstname newLastname:(NSString*)newLastname newAddress:(NSString*)newAddress newAge:(int)newAge newPhoneNumber:(NSString*)newPhoneNumber newProfilePicURL:(NSString*)newProfilePicURL
 {
-    return true;
+    self.firstname = newFirstname;
+    self.lastname = newLastname;
+    self.address = newAddress;
+    
+    self.age = newAge;
+    self.phoneNumber = newPhoneNumber;
+    self.profilePicURL = newProfilePicURL;
+    
+    
+}
+
+-(void) saveProfile
+{
+    NSLog(@"\nAccount has been successfully updated!\n");
+}
+
+-(void) printProfile
+{
+    NSLog(@"\n==============================\n");
+    
+    NSLog(@"Firstname is %@\n", self.firstname);
+    NSLog(@"Lastname is %@\n", self.lastname);
+    
+    NSLog(@"Address is %@\n", self.address);
+    NSLog(@"Age is %d\n", self.age);
+    
+    NSLog(@"Phone# is %@\n", self.phoneNumber);
+    NSLog(@"My Profile Picture is located at this link %@\n", self.profilePicURL);
+    NSLog(@"\n==============================\n");
 }
 
 @end
