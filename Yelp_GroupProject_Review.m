7c90975429a2473a2a71c09003fd792af1a25269
Hannah Park <teyang610@gmail.com>
2017-02-23 21:28:37 -0800

    Create Yelp_GroupProject_Review.m

---
+++ Yelp_GroupProject_Review.m
@@ -0,0 +1,18 @@
+//
+//  Review.m
+//  
+//
+//  Created by HannahPark on 2017-02-22.
+//
+//
+
+#import "Review.h"
+
+@implementation Review
+
+-(id) initWithSeeReviewing:(NSMutableArray<SeeReviewing*>*)SeeReviewing WriteReviewing:(NSMutableArray<WriteReviewing*>*) WriteReviewing Review:(NSMutableArray<Review*>*) Review
+{
+    
+}
+
+@end
