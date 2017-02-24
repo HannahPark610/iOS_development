c634a93fa744a01e8fd13aaf63f1be3459c658d6
Hannah Park <teyang610@gmail.com>
2017-02-23 21:28:14 -0800

    Create Yelp_GroupProject_Review.h

---
+++ Yelp_GroupProject_Review.h
@@ -0,0 +1,34 @@
+//
+//  Review.h
+//  
+//
+//  Created by HannahPark on 2017-02-22.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "SeeReviewing.h"
+#import "WriteReviewing.h"
+#import "Review.h"
+
+@interface Review : NSObject
+
+
+//properties
+@property (strong, nonatomic) NSMutableArray<SeeReviewing*>* SeeReviewing;
+@property (strong, nonatomic) NSMutableArray<WriteReviewing*>* WriteReviewing;
+@property (strong, nonatomic) NSMutableArray<Review*>* Review;
+
+
+
+//constructors
+-(id) initWithSeeReviewing:(NSMutableArray<SeeReviewing*>*)SeeReviewing WriteReviewing:(NSMutableArray<WriteReviewing*>*) WriteReviewing Review:(NSMutableArray<Review*>*) Review;
+
+
+
+//methods (APIs)
+
+
+
+
+@end
