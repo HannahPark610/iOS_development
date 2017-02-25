7286a47c36fa52fe71c962dd5db94fb0ad11f241
Hannah Park <teyang610@gmail.com>
2017-02-24 21:18:08 -0800

    Create Yelp_GroupProject_SeeReviewing.m

---
+++ Yelp_GroupProject_SeeReviewing.m
@@ -0,0 +1,13 @@
+//
+//  SeeReviewing.m
+//  yelp
+//
+//  Created by HannahPark on 2017-02-22.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import "SeeReviewing.h"
+
+@implementation SeeReviewing
+
+@end
