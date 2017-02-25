a7973654f0f6ba9522afe22ea20c0bd5e2660209
Hannah Park <teyang610@gmail.com>
2017-02-24 21:16:29 -0800

    Create Yelp_GroupProject_AdditionalTip.m

---
+++ Yelp_GroupProject_AdditionalTip.m
@@ -0,0 +1,13 @@
+//
+//  AdditionalTip.m
+//  yelp
+//
+//  Created by HannahPark on 2017-02-22.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import "AdditionalTip.h"
+
+@implementation AdditionalTip
+
+@end
