642bce4f1704a57e31c2c99b7cd10e433b060527
Hannah Park <teyang610@gmail.com>
2017-02-24 21:15:02 -0800

    Create Yelp_GroupProject_WriteReviewing.h

---
+++ Yelp_GroupProject_WriteReviewing.h
@@ -0,0 +1,24 @@
+//
+//  WriteReviewing.h
+//  yelp
+//
+//  Created by HannahPark on 2017-02-22.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PostComment.h"
+
+@interface WriteReviewing : NSObject
+
+//properties
+
+@property 
+
+
+//constructors
+
+
+//methods (APIs)
+
+@end
