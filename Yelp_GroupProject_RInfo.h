a3a1d2cfc06645e2148ea14e7fd77e252d534dc4
Hannah Park <teyang610@gmail.com>
2017-02-24 21:16:55 -0800

    Create Yelp_GroupProject_RInfo.h

---
+++ Yelp_GroupProject_RInfo.h
@@ -0,0 +1,13 @@
+//
+//  RInfo.h
+//  yelp
+//
+//  Created by HannahPark on 2017-02-22.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RInfo : NSObject
+
+@end
