69786918f258252c495d7cbe9ab61444cdd8903a
Hannah Park <teyang610@gmail.com>
2017-02-24 21:17:15 -0800

    Create Yelp_GroupProject_RInfo.m

---
+++ Yelp_GroupProject_RInfo.m
@@ -0,0 +1,13 @@
+//
+//  RInfo.m
+//  yelp
+//
+//  Created by HannahPark on 2017-02-22.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import "RInfo.h"
+
+@implementation RInfo
+
+@end
