49fd2a55605ac0df6e7dc0bfe0ac9803bad644cf
Hannah Park <teyang610@gmail.com>
2017-02-24 21:16:12 -0800

    Create Yelp_GroupProject_AdditionalTip.h

---
+++ Yelp_GroupProject_AdditionalTip.h
@@ -0,0 +1,13 @@
+//
+//  AdditionalTip.h
+//  yelp
+//
+//  Created by HannahPark on 2017-02-22.
+//  Copyright © 2017 HannahPark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdditionalTip : NSObject
+
+@end
